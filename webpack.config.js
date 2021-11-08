c9367fe678865cade672b88158165f093c1878ed
senbar <sendek.bartosz@gmail.com>
2021-11-08 18:11:25 +0100

    fix package naming; serve alias to main js

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const singleSpaDefaults = require("webpack-config-single-spa");
 module.exports = (webpackConfigEnv, argv) => {
   const defaultConfig = singleSpaDefaults({
     orgName: "Sigma",
-    projectName: "SoeAuth",
+    projectName: "auth",
     webpackConfigEnv,
     argv,
   });
